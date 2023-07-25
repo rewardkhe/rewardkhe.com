0f0159a609fd0c965f0ac07f4402af82c5841fff
Reward Khe <rewardkhe@outlook.com>
2023-07-25 21:10:31 +1000

    footer's src updated

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -3,12 +3,14 @@ export function Footer(){
 <>
 <footer>
         <div className='animal'>
-        <div> <video width="50px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(1).mp4" type="video/mp4"/> </video> </div>
-        <div> <video width="60px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(2).mp4" type="video/mp4"/> </video> </div>
-        <div> <video width="80px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(3).mp4" type="video/mp4"/> </video> </div>
-        <div> <video width="70px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(4).mp4" type="video/mp4"/> </video> </div>
-        <div> <video width="80px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(5).mp4" type="video/mp4"/> </video> </div>
-        <div> <video width="30px" height="auto" muted autoPlay loop playsInline> <source src = "Public/f(6).mp4" type="video/mp4"/> </video> </div>
+        <div> <video width="50px" height="auto" muted autoPlay loop playsInline> <source src = '/public/f(1).mp4' type="video/mp4"/> </video> </div>
+        <div> <video width="60px" height="auto" muted autoPlay loop playsInline> <source src = "/f(2).mp4" type="video/mp4"/> </video> </div>
+        <div> <video width="80px" height="auto" muted autoPlay loop playsInline> <source src = "/f(3).mp4" type="video/mp4"/> </video> </div>
+        <div> <video width="70px" height="auto" muted autoPlay loop playsInline> <source src = "/f(4).mp4" type="video/mp4"/> </video> </div>
+        <div> <video width="80px" height="auto" muted autoPlay loop playsInline> <source src = "/f(5).mp4" type="video/mp4"/> </video> </div>
+        <div> <video width="30px" height="auto" muted autoPlay loop playsInline> <source src = "/public/f(6).mp4" type="video/mp4"/> </video> </div>
+        <source src=" /public/f(1).mp4" />
+
         </div>
    </footer>
 </>
