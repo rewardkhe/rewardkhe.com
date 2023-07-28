70f25ae56251bd51ba402351374ebb00406c6320
Reward Khe <rewardkhe@outlook.com>
2023-07-28 23:06:12 +1000

    changed from browser to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import './FirstGlance-css.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
+    {/* <BrowserRouter>  */}
     <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 )
